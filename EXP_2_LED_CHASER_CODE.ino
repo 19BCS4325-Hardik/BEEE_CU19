c19fa882404e28cedc77d36f6bc604f58ca5c678
Hardik1809-coder <54958082+Hardik1809-coder@users.noreply.github.com>
2019-09-18 08:21:41 -0600

    Add files via upload

---
+++ EXP_2_LED_CHASER_CODE.ino
@@ -0,0 +1,39 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(9,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(2,HIGH);
+delay(1000);
+digitalWrite(2,LOW);
+digitalWrite(3,HIGH);
+delay(1000);
+digitalWrite(3,LOW);
+digitalWrite(4,HIGH);
+delay(1000);
+digitalWrite(4,LOW);
+digitalWrite(5,HIGH);
+delay(1000);
+digitalWrite(5,LOW);
+digitalWrite(6,HIGH);
+delay(1000);
+digitalWrite(6,LOW);
+digitalWrite(7,HIGH);
+delay(1000);
+digitalWrite(7,LOW);
+digitalWrite(8,HIGH);
+delay(1000);
+digitalWrite(8,LOW);
+digitalWrite(9,HIGH);
+delay(1000);
+digitalWrite(9,LOW);
+}
